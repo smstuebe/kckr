8ae8833687f1a64e52dadcfad4b9b11c48f20da9
Sven-Michael Stübe <stuebe2k7@gmail.com>
2019-04-24 00:36:18 +0200

    opencv setup part 1

--- device/scripts/setup.sh
+++ device/scripts/setup.sh
@@ -1,5 +1,6 @@
 # config ###############################################################################
 PYTHON_VERSION=3.7.3
+OPENCV_VERSION=4.1.0
 
 # general ##############################################################################
 sudo apt-get update -y
@@ -51,7 +52,63 @@ if [ ! -d "grovepi" ]; then
     ./grovepi/Script/firmware_update.sh
 fi
 
+# opencv ###############################################################################
+# todo: if
+# todo enlarge disk space
+# https://www.pyimagesearch.com/2018/09/26/install-opencv-4-on-your-raspberry-pi/
+
+sudo apt-get install build-essential cmake unzip pkg-config -y
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+
+#sudo apt-get install libgtk-3-dev
+#sudo apt-get install libcanberra-gtk*
+
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install python3-dev -y
+
+wget -O opencv.zip https://github.com/opencv/opencv/archive/$OPENCV_VERSION.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/$OPENCV_VERSION.zip
+
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+mv opencv-$OPENCV_VERSION opencv
+mv opencv_contrib-$OPENCV_VERSION opencv_contrib
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D BUILD_EXAMPLES=OFF ..
+
+# todo change swap size
+#sudo nano /etc/dphys-swapfile
+#CONF_SWAPSIZE=2048
+#sudo /etc/init.d/dphys-swapfile stop
+#sudo /etc/init.d/dphys-swapfile start
+
+make -j4
+sudo make install
+sudo ldconfig
+
+#todo change swap size back
+#sudo nano /etc/dphys-swapfile
+#CONF_SWAPSIZE=100
+#sudo /etc/init.d/dphys-swapfile stop
+#sudo /etc/init.d/dphys-swapfile start
+
 # kicker activity stuff ################################################################
+
 #todo
 #pip install -r /mnt/device/scripts/requirements.txt --user
 
